3dec53ac7af9846631f7b98fc03364129af9e2fe
DMustache <dvusakov@gmail.com>
2020-11-24 13:00:50 +0300

    1 programming

---
+++ Python/Tkinter/Weather.py
@@ -0,0 +1,44 @@
+from tkinter import Tk
+from tkinter import messagebox
+from tkinter import Canvas
+from tkinter import Frame
+from tkinter import Label
+from tkinter import Entry
+from tkinter import Button 
+import requests
+
+root = Tk()
+
+def get_weather():
+    city = cityField.get()
+
+    key = '797261b25ff6a499d71adea747c337cf'
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+
+    params = {'APPID': key, 'q': city, 'units': 'imperial'}
+    result = requests.get(url, params=params)
+    weather = result.json()
+    print(weather)
+
+root['bg'] = '#fafafa'
+root.title('Weather control')
+root.geometry('300x250')
+
+root.resizable(width=False, height=False)
+
+frame_top = Frame(root, bg='#ffb700', bd=5)
+frame_top.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.25)
+
+frame_bottom = Frame(root, text='Show weather', bd=5)
+frame_top.place(relx=0.15, rely=0.55, relwidth=0.7, relheight=0.1)
+
+cityField = Entry(frame_top, bg='white', font=30)
+cityField.pack()
+
+btn = Button(frame_top, text='Watch weather', command=get_weather)
+btn.pack()
+
+info = Label(frame_bottom, text='Weather info', bg='#ffb700', font=40)
+info.pack()
+
+root.mainloop()
---
+++ ЕГЭ/ЕГЭ 27/01.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+void gen()
+{
+    ofstream fs("data.txt");
+    mt19937 mt(time(0));
+    uniform_int_distribution<> dqtty(1, 400);
+    uniform_int_distribution<> dnumber(1, 1000);
+    int n = dqtty(mt);
+    cout << n << endl;
+    for(int i = 0; i < n; i++)
+        fs << dnumber(mt) << endl;
+}
+
+void ineff()
+{
+    int n = 0;
+    int mx = 0;
+    cin >> n;
+    vector<int> nums(n, 0);
+    for (int i = 0; i< n; i++)
+        cin >> nums[i];
+    for (int i = 0; i < n; i++)
+        for(int j = 1; j < n; j+ 6)
+            if((nums[i] + nums[j]) % 19 == 0)
+                mx = max(mx, nums[i] + nums[j]);
+    cout << mx << endl;
+}
+
+void eff()
+{
+    int n = 0;
+    int mx = 0;
+    cin >> n;
+    vector<int> nums(n, 0);
+    for (int i = 0; i< n; i++)
+        cin >> nums[i];
+    for (int i = 0; i < n; i++)
+        for(int j = 1; j < n; j+ 6)
+            if((nums[i] + nums[j]) % 19 == 0)
+                mx = max(mx, nums[i] + nums[j]);
+    cout << mx << endl;
+}
+
+int main()
+{
+    /*int mode(0);
+    cin >> mode;
+    switch(mode)
+    {
+        case 0: gen(); break;
+        case 1: ineff(); break;
+        case 2: eff; break;
+        default: cout << "ERROR" << endl;
+
+    }*/
+ofstream f1("data.txt");
+f1 << 5;
+f1.close();
+ifstream f2("data.txt");
+int k = 0;
+f2 >> k;
+cout << k << endl;
+f2.close();
+}
