45f6b4f6adfba6bfad112a9bb86aa39b764e3100
DMustache <dvusakov@gmail.com>
2020-12-03 21:17:45 +0300

    03 12 2020

--- Python/Tkinter/translator.py
+++ Python/Tkinter/translator.py
@@ -5,6 +5,9 @@ from tkinter import Label
 from tkinter import Frame
 from tkinter import OptionMenu
 from tkinter import StringVar
+from tkinter import TOP
+from tkinter import LEFT
+from tkinter import RIGHT
 
 from googletrans import Translator
 
@@ -16,10 +19,35 @@ root.geometry('450x300')
 
 root.resizable(width=False, height=False)
 
-# frame_top = Frame(root, bg='#1a73e8', bd=5)
-# frame_top.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.25)
+frame_top = Frame(root, bg='#1a73e8', bd=5)
+frame_top.place(relx=0.05, rely=0, relwidth=0.9, relheight=0.15)
 
 frame_bottom = Frame(root, bg='#1a73e8', bd=5)
-frame_bottom.place(relx=0.05, rely=0, relwidth=0.9, relheight=0.8)
+frame_bottom.place(relx=0.05, rely=0.2, relwidth=0.9, relheight=0.8)
+
+LANGUAGESPL = [
+    'Русский',
+    'Английский'
+]
+
+variablePL = StringVar(root)
+variablePL.set(LANGUAGESPL[1])
+
+LANGUAGESTL = [
+    'Русский',
+    'Английский'
+]
+
+variableTL = StringVar(root)
+variableTL.set(LANGUAGESTL[0])
+
+PhraseLang = OptionMenu(frame_top, variableTL, *LANGUAGESPL)
+PhraseLang.place(relx=0.1)
+
+RevButton = Button(frame_top, text='Rotate lang')
+RevButton.place(relx=0.45)
+
+TranslatedLang = OptionMenu(frame_top, variablePL, *LANGUAGESTL)
+TranslatedLang.place(relx=0.65)
 
 root.mainloop()
