139f07d19670a3cf908e8c33fcfc3f894d231359
Dmitry Mustache <63397857+DMustache@users.noreply.github.com>
2021-02-01 10:09:35 +0300

    ыугшвчстгшВТЧгшвщфыЧЬ

--- Python/Алгоритмы/abstract classes/Школьное Шифрование/letter dechipher.py
+++ Python/Алгоритмы/abstract classes/Школьное Шифрование/letter dechipher.py
@@ -1,4 +1,3 @@
-import pymorphy2
 f = open('Python\\Алгоритмы\\abstract classes\\Школьное Шифрование\\letter.txt', encoding='utf-8')
 letter = str(f.read())
 f.close()
@@ -21,7 +20,6 @@ popularInLetter = [' ' for i in range(len(popularAbc))]
 for i in range(len(popularAbc)):
     popularInLetter[i] = abc[abcRatio.index(max(abcRatio))]
     abcRatio[abcRatio.index(max(abcRatio))] = 0
-
 for i in letter:
     if i in popularInLetter:
         i = popularAbc[popularInLetter.index(i)]
@@ -31,18 +29,23 @@ f.write(letter)
 f.close()
 
 #       abc = ['а','б','в','г','д','е','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
-abcToChange = [' ',' ',' ',' ','д',' ',' ',' ',' ',' ',' ',' ','м',' ','о',' ',' ',' ',' ',' ',' ','ж',' ',' ','и',' ',' ',' ',' ',' ',' ','я']
+abcToChange = [' ',' ',' ',' ','д',' ','й',' ',' ',' ',' ',' ','м',' ','о',' ',' ',' ',' ',' ',' ','ж',' ',' ','и',' ',' ',' ',' ',' ',' ','я']
+
+for i in abcToChange:
+    if abcToChange.count(i) > 1 and i != ' ':
+        print(f'{i} в алфавите встречается {abcToChange.count(i)} раз')
 
 for i in letter:
     if i in abc and abcToChange[abc.index(i)] != ' ':
         i = abcToChange[abc.index(i)]
 
-f = open('Python\\Алгоритмы\\abstract classes\\Школьное Шифрование\\newCOPY.txt', 'w', encoding='utf-8')
-f.write(letter)
-f.close()
-
 with open('Python\\Алгоритмы\\abstract classes\\Школьное Шифрование\\findWords.txt', 'r', encoding='utf-8') as f:
     findWords = f.readlines()
 findWords = [x.strip() for x in findWords]
 f.close()
+
+f = open('Python\\Алгоритмы\\abstract classes\\Школьное Шифрование\\newCOPY.txt', 'w', encoding='utf-8')
+f.write(letter)
+f.close()
+
 print("end")
