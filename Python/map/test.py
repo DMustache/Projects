1d3398b8a3340c684ffed3c7c5f66eed514d5841
DMustache <dvusakov@gmail.com>
2021-02-22 16:45:10 +0300

    script work!!!

--- Python/map/test.py
+++ Python/map/test.py
@@ -10,50 +10,39 @@ spd = 0.5
 rot_spd = 0.1
 
 numList = []
-# def createMap(MAP):
-#     mapsize = 50
-#     def adjacent_min(noise):
-#         output = []
-#         for i in range(len(noise) - 1):
-#             output.append(min(noise[i], noise[i + 1]))
-#         return output
+def createMap(MAP):
+    mapsize = 50
+    def adjacent_min(noise):
+        output = []
+        for i in range(len(noise) - 1):
+            output.append(min(noise[i], noise[i + 1]))
+        return output
+    global numList
+    for i in range(50):
+        random.seed(i)
+        noise = [random.randint(1, 2) for i in range(mapsize)]
+        numList.append(adjacent_min(noise))
+    counter = 0
+    for i in range(len(numList)):
+        for j in range(len(numList)):
+            try:
+                if numList[i][j] == 1:
+                    MAP += '.'
+                    if len(MAP) % 50 + counter == 0:
+                        counter += 1
+                        MAP += '\n'
+                else:
+                    MAP += '#'
+                    if len(MAP) % 50 + counter == 0:
+                        counter += 1
+                        MAP += '\n'
+            except IndexError:
+                continue
+    return MAP
 
-#     global numList
-#     for i in range(50):
-#         random.seed(i)
-#         noise = [random.randint(1, 2) for i in range(mapsize)]
-#         numList.append(adjacent_min(noise))
-#     counter = 0
-#     for i in range(len(numList)):
-#         for j in range(len(numList)):
-#             try:
-#                 if numList[i][j] == 1:
-#                     MAP += '.'
-#                     if len(MAP) % 50 + counter == 0:
-#                         counter += 1
-#                         MAP += '\n'
-#                 else:
-#                     MAP += '#'
-#                     if len(MAP) % 50 + counter == 0:
-#                         counter += 1
-#                         MAP += '\n'
-#             except IndexError:
-#                 continue
-#     return MAP
+MAP = ''
+MAP = createMap(MAP)
 
-MAP = '''
-    ############
-    #..........#
-    #..........#
-    #....#.....#
-    #....##....#
-    #.....#....#
-    #..........#
-    #..........#
-    ############
-    '''
-#MAP = createMap(MAP)
-raise print(len(MAP))
 def make_map(string_map):
     rows = string_map.strip().split('\n')
     h = len(rows)
