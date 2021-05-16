5caca114e6df41675bbdd20f5d46772b758f254a
Dmitry Mustache <63397857+DMustache@users.noreply.github.com>
2021-05-17 00:02:02 +0300

    end

--- CF/Футбол.py
+++
@@ -1,9 +0,0 @@
-def fuc(n):
-    if n == 0:
-        return 1
-    return fuc(n-1) * n
-
-n = int(input())
-c1 = fuc(n)//(fuc(n-11)*fuc(11))
-c2 = fuc(n-11)//(fuc(n-22)*fuc(11))
-print((c1*c2//2)%(1000000007))
--- Пост Новогоднее введение/a1.py
+++
@@ -1,6 +0,0 @@
-maxNum, brake, multiply = map(int, input().split())
-
-counter = 0
-tmp = 1
-for i in range(brake):
-    
